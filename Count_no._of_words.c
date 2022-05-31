ddfbd7ced401774c122a572c66262f5b0da09ad8
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-05-31 19:13:28 +0530

    #include<stdio.h> int main() {     int i,c=0;     char s[100];     scanf("%[^ ]s",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==' ')         {             c++;         }     }     printf("%d",c+1); }

---
+++ Count_no._of_words.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,c=0;
+    char s[100];
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c+1);
+}
