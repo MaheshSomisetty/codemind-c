4eb8f0f3701ef8acf2a6f8822f0a30e561dd9e3c
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-06-08 18:08:04 +0530

    #include<stdio.h> int main() {     int n,a[100],i,ind,min,max;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     min=a[0];     for(i=0;i<n;i++)     {         if(min>a[i])         {             min=a[i];             ind=i;             break;         }     }     max=min;     for(i=ind;i<n;i++)     {         if(max<a[i])         {             max=a[i];         }     }     printf("%d",max-min); }

---
+++ Best_Time_to_Buy_and_Sell_Stock.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,ind,min,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>a[i])
+        {
+            min=a[i];
+            ind=i;
+            break;
+        }
+    }
+    max=min;
+    for(i=ind;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max-min);
+}
