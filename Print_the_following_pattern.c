a11185240784c0a6de489ddf8032dad2a28babf7
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-21 21:18:44 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=n;j++)         {             if(j+i==n+1||j-i==0)             {                 printf("%d ",i);             }             else             {                 printf(" ");             }         }         printf(" ");     }     return 0; }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j+i==n+1||j-i==0)
+            {
+                printf("%d ",i);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+    return 0;
+}
